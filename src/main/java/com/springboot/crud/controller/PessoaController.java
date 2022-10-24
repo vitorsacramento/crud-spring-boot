858dea057e469d3db64573c4b1ce199ceacc008b
Vitor Sacramento <vitorsacramento85@gmail.com>
2022-10-24 20:45:57 -0300

    Implementação endpoint adicionar

--- src/main/java/com/springboot/crud/controller/PessoaController.java
+++ src/main/java/com/springboot/crud/controller/PessoaController.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,5 +37,10 @@ public class PessoaController {
 		}
 		
 		return ResponseEntity.ok(pessoa.get());
+	}
+	
+	@PostMapping
+	public Pessoa adicionar(@RequestBody Pessoa pessoa) {
+		return pessoaRepository.save(pessoa);
 	}
 }
